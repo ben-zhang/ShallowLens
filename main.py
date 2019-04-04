f0fc35265d3119f488ad904547e0b04cdab73b77
Ben Zhang <benzhang@tribalscale.com>
2019-04-04 15:51:47 -0400

    add gaussian blur before building edge map

--- main.py
+++ main.py
@@ -3,26 +3,25 @@ import numpy as np
 
 # We always need np with cv2, since images in opencv are np arrays.
 
-img_path = "./images/circle.png"
+img_path = "./images/ouchmouse.jpg"
 
-img = cv2.imread(img_path)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img_circle_path = "./images/circle.png"
 
-gray = np.float32(gray)
+img = cv2.imread(img_circle_path)
+
+imgBlur = cv2.blur(img, (7, 7))
 
 """
 args: image, lower bound, upper bound, Sobel kernel size, L2Gradient (bool)
 The lower and upper bounds are on the intensity gradient.
 Within this range, things are edgy.
 """
-edgeMap = cv2.Canny(img, 100, 200)
+edgeMap = cv2.Canny(imgBlur, 100, 200)
 
-corners = cv2.cornerHarris(gray, 2, 3, 0.04)
+corners = cv2.cornerHarris(edgeMap, 2, 3, 0.04)
 
 highlightCorners = cv2.dilate(corners, None)
 img[corners>0.01*corners.max()] = [0, 0, 255]
 
 cv2.imshow("corners", img)
 cv2.waitKey(0)
-
-# should filter out corners that aren't cornery enough
