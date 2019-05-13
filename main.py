d4c26d741bafa0fb28f7d5d61a8deda38733cfe0
Ben <benzhang42@gmail.com>
2019-05-13 00:29:47 -0400

    deleted edge detection approach

--- main.py
+++ main.py
@@ -3,25 +3,12 @@ import numpy as np
 
 # We always need np with cv2, since images in opencv are np arrays.
 
+
 img_path = "./images/ouchmouse.jpg"
 
 img_circle_path = "./images/circle.png"
 
 img = cv2.imread(img_circle_path)
 
-imgBlur = cv2.blur(img, (7, 7))
 
-"""
-args: image, lower bound, upper bound, Sobel kernel size, L2Gradient (bool)
-The lower and upper bounds are on the intensity gradient.
-Within this range, things are edgy.
-"""
-edgeMap = cv2.Canny(imgBlur, 100, 200)
 
-corners = cv2.cornerHarris(edgeMap, 2, 3, 0.04)
-
-highlightCorners = cv2.dilate(corners, None)
-img[corners>0.01*corners.max()] = [0, 0, 255]
-
-cv2.imshow("corners", img)
-cv2.waitKey(0)
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 # ShallowLens
 Building an object detection algorithm without neural nets
 
+## This project is based on the research done by Li et al in the following paper:
+Salient region detection based on the superpixel and texture
+https://ieeexplore-ieee-org.proxy.lib.uwaterloo.ca/document/8301982
+
+Correct citations coming later.
