1e0688bbe17fdba8c76ff476d7c25818121e428b
Ben <benzhang42@gmail.com>
2019-06-08 19:49:27 -0400

    rip

--- classifier.py
+++ classifier.py
@@ -1,6 +1,7 @@
 import cv2
 from cv2.ximgproc import createSuperpixelSLIC
 from cv2.ximgproc import SLIC as SLIC_CONST
+from skimage.segmentation import slic
 import numpy as np
 
 # We always need np with cv2, since images in opencv are np arrays.
@@ -12,12 +13,5 @@ def pre_process(img):
     # SLIC clusters on values (L, A, B, X, Y)
     img_lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
 
-    slic = createSuperpixelSLIC(img_lab, SLIC_CONST, region_size, ruler)
-
-
-
-    slic.iterate(10)
-
-    superpixels = slic.getLabelContourMask(True)
-    cv2.imshow("img", superpixels)
-    cv2.waitKey(0)
+    # Cluster and extract cluster centers
+    superpixels = slic()
